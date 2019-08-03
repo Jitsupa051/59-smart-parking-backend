8f81133f22c720b5eb98f0dca6d0312ce1ef7274
Jitsupa051 <42166811+Jitsupa051@users.noreply.github.com>
2019-08-04 00:41:20 +0700

    update

--- api/parking.php
+++ api/parking.php
@@ -4,6 +4,18 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 $app->post('/parking', function (Request $request, Response $response) {
     $data = $request->getParsedBody();
-    $response->getBody()->write("Hello, $data[name] $data[age]");
-    return $response;
+    $database = $GLOBALS['dbconn'];
+
+    $result = $database->insert('parking', [
+        'pname' => $data['pname'],
+        'possition' => $data['possition'],
+        'pImage' => $data['pImage'],//ติดไว้ก่อน
+        'puuid' => $data['puuid'],
+        'ename' => $data['ename']
+
+    ]);
+
+
+
+    return $response->withJson($result->rowCount(),200);
 });
